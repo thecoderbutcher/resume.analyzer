13bcf1d089e5aa3f8c44eaaa84ff6dc32a401e9a
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-10-19 20:52:50 -0300

    add title section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Title from "./components/Title"
 import UploadReseume from "./components/UploadReseume"
 
 function App() {
 
   return (
     <main className="flex flex-col w-screen h-screen bg-gray-900 items-center justify-center text-slate-400">
+      <Title/>
       <UploadReseume />
     </main>
   )
---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+const Title = () => {
+  return (
+    <div className="flex flex-col gap-4 mb-8 items-center justify-center">
+      <h1 className="text-6xl font-semibold">Resume Analyzer</h1>
+      <p className="font-light">
+        Upload your pdf resume an get a instant AI feedback
+      </p>
+    </div>
+  );
+};
+
+export default Title;
