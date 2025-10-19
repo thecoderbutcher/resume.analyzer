d1e684927957f8d46d87386149dd344a74d0e457
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-10-19 20:39:37 -0300

    add upload file view

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import UploadReseume from "./components/UploadReseume"
 
 function App() {
 
   return (
-    <main className="flex flex-col w-screen h-screen bg-gray-900">
-
+    <main className="flex flex-col w-screen h-screen bg-gray-900 items-center justify-center text-slate-400">
+      <UploadReseume />
     </main>
   )
 }
