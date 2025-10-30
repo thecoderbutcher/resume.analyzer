50c92ff05ca3150b40c177ce578415e7425d4bde
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-10-30 12:18:00 -0300

    update general components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,38 @@
 import { images } from "../../constants/language";
 import { useLangStore } from "../../store/useLangStore";
+import { ROUTES } from "../../constants/routes";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { lang, setLang } = useLangStore();
   return (
-    <nav className="flex h-16 w-full justify-end gap-2 items-center px-6 ">
-      <img
-        src={images.SpanishIcon}
-        alt=""
-        className={`${lang !== "es" ? "grayscale" : ""} transition-all duration-300`}
-        onClick={() => {
-          setLang("es");
-        }}
-      />
-      <img
-        src={images.EnglishIcon}
-        alt=""
-        className={`${lang !== "en" ? "grayscale" : ""} transition-all duration-300`}
-        onClick={() => {
-          setLang("en");
-        }}
-      />
+    <nav className="flex h-16 w-full justify-between items-center px-6 ">
+      <div>logo</div>
+      <div className="flex gap-4">
+        {ROUTES.map((item, index) => (
+          <Link to={item.path} key={index}>
+            {item.label[lang]}
+          </Link>
+        ))}
+      </div>
+      <div className="flex gap-2">
+        <img
+          src={images.SpanishIcon}
+          alt=""
+          className={`${lang !== "es" ? "grayscale" : ""} transition-all duration-300`}
+          onClick={() => {
+            setLang("es");
+          }}
+        />
+        <img
+          src={images.EnglishIcon}
+          alt=""
+          className={`${lang !== "en" ? "grayscale" : ""} transition-all duration-300`}
+          onClick={() => {
+            setLang("en");
+          }}
+        />
+      </div>
     </nav>
   );
 };
