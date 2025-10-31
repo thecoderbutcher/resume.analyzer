62c5d2d3489168fb811ec025c0493b55e9208784
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-10-31 12:18:58 -0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useEffect } from "react";
-import { useCvStore } from "../store/useCvStore";
-import LoadingResume from "./components/LoadingResume";
-import Result from "./components/Result";
-import Title from "./components/Title";
-import UploadResume from "./components/UploadResume";
-import Navbar from "./components/Navbar";
+import Layout from "./layout";
+import AppRoutes from "../routes/AppRoutes";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const { setAIReady, isLoading, uploadedFile } = useCvStore();
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (window.puter?.ai?.chat) {
-        setAIReady(true);
-        clearInterval(interval);
-      }
-    }, 300);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <main className="flex flex-col min-h-screen transition-all duration-300">
-      <Navbar />
-      <div className="flex flex-col grow-1 items-center justify-center">
-        <Title />
-        {!uploadedFile && <UploadResume />}
-        {isLoading && <LoadingResume />}
-        {uploadedFile && !isLoading && <Result />}
-      </div>
-    </main>
+    <BrowserRouter>
+      <Layout>
+        <AppRoutes />
+      </Layout>
+      ;
+    </BrowserRouter>
   );
 }
 
