eb0325f32159c6b88f305877f0aec72f7337fe9e
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-10-31 12:18:30 -0300

    create layout

---
+++ src/layout.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+const Layout = ({ children }) => {
+  return (
+    <main className="flex flex-col min-h-screen transition-all duration-300">
+      <Navbar />
+      <div className="flex flex-col grow-1 items-center justify-center">
+        {children}
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
