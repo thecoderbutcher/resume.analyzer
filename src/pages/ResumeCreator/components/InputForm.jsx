6b5df56275e2c9f5a867f99dc0408fef91518d54
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-11-06 08:28:59 -0300

    add input component

---
+++ src/pages/ResumeCreator/components/InputForm.jsx
@@ -0,0 +1,15 @@
+const InputForm = ({ error, errormessage, children, Icon }) => {
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <div
+        className={`flex items-center w-full bg-gray-700 px-2 py-1 rounded-md ${error ? "border border-red-600/50" : ""}`}
+      >
+        {children}
+        <Icon className="text-gray-400" />
+      </div>
+      {error && <p className="text-red-500 text-xs">{errormessage}</p>}
+    </div>
+  );
+};
+
+export default InputForm;
