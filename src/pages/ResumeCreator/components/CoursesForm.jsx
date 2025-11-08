ca0d4b436464d23b3b57b7e62b720d7dc46090c8
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-11-08 09:40:42 -0300

    add course form

---
+++ src/pages/ResumeCreator/components/CoursesForm.jsx
@@ -0,0 +1,32 @@
+import { useFormStore } from "../../../../store/useFormStore";
+
+const CoursesForm = () => {
+  const { nextStep, prevStep } = useFormStore();
+  return (
+    <div>
+      <div className="flex flex-col space-y-4">
+        <h2>Cursos</h2>
+        <input type="text" placeholder="titulo" />
+        <input type="text" placeholder="institucion" />
+        <input type="text" placeholder="estado" />
+        <button>Agregar mas</button>
+      </div>
+      <div className="flex justify-between pt-8">
+        <button
+          className="px-2 py-1 bg-blue-500/50 rounded-md hover:bg-blue-500 transition-all duration-200"
+          onClick={() => prevStep()}
+        >
+          Atras
+        </button>
+        <button
+          className="px-2 py-1 bg-blue-500/50 rounded-md hover:bg-blue-500 transition-all duration-200"
+          type="submit"
+        >
+          Finalizar
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CoursesForm;
