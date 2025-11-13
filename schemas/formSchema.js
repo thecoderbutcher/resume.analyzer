c040630b69a04734a0cf463e0ee411be5d3d727f
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-11-13 12:49:10 -0300

    add zod schemas form validations

--- schemas/formSchema.js
+++ schemas/formSchema.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const personalInformationSchema = z.object({
   fullname: z.string().min(4, "name error"),
-  position: z.string().optional(),
+  role: z.string().optional(),
   phone: z.string().min(11, "Ingresar un telefono valido"),
   email: z.email("Email invalido"),
   web: z.string().optional(),
@@ -11,17 +11,22 @@ export const personalInformationSchema = z.object({
 });
 
 export const experienceSchema = z.object({
-  position: z.string().optional(),
-  startdate: z.date().optional(),
-  enddate: z.date().optional(),
-  role: z.string().optional(),
-  company: z.string().optional(),
-  description: z.string().optional(),
+  position: z.string().min(3, "campo obligatorio"),
+  company: z.string().min(3, "campo obligatorio"),
+  startdate: z.string().min(3, "EL campo no puede ser vacio").optional(),
+  enddate: z.string().optional(),
+  workDescription: z.string().optional(),
 });
 
 export const educationSchema = z.object({
-  degree: z.string().optional(),
-  institution: z.string().optional(),
-  state: z.string().optional(),
-  description: z.string().optional(),
+  degree: z.string().min(4, "compo obligatorio"),
+  institution: z.string().min(4, "campo obligatorio"),
+  state: z.string().min(4, "compo obligatorio"),
+  hours: z.coerce.number().optional(),
+  educationDescription: z.string().optional(),
+});
+
+export const extraSchema = z.object({
+  language: z.string().min(3, "campo obligatorio"),
+  level: z.string().min(3, "campo obligatorio"),
 });
