1414eff018979290e6f3a669a1f57ef837b3ad8f
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-11-21 12:57:29 -0300

    update resume creator index

--- src/pages/ResumeCreator/index.jsx
+++ src/pages/ResumeCreator/index.jsx
@@ -7,12 +7,27 @@ import { creatorTitle } from "../../../constants/language";
 import PersonalForm from "./components/PersonalForm";
 import ExperienceForm from "./components/ExperienceForm";
 import EducationForm from "./components/EducationForm";
-import CoursesForm from "./components/CoursesForm";
+import ExtraForm from "./components/ExtraForm";
+import { useEffect } from "react";
+import { useCvStore } from "../../../store/useCvStore";
+import Loading from "../../components/Loading";
+import { loadingCreate } from "../../../constants/language";
+import GeneratePdf from "./components/GeneratePdf";
 import ControlButtons from "./components/ControlButtons";
 
 const ResumeCreator = () => {
-  const { step } = useFormStore();
   const { lang } = useLangStore();
+  const { step } = useFormStore();
+  const { setAIReady, isLoading, isGenerate } = useCvStore();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (window.puter?.ai?.chat) {
+        setAIReady(true);
+        clearInterval(interval);
+      }
+    }, 300);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="px-2">
@@ -20,15 +35,28 @@ const ResumeCreator = () => {
         title={creatorTitle[lang].title}
         subtitle={creatorTitle[lang].subtitle}
       />
-      <Card>
-        <Stepper currentStep={step} />
-        <div className="flex flex-col p-2 md:p-6">
-          {step === 1 && <PersonalForm />}
-          {step === 2 && <ExperienceForm />}
-          {step === 3 && <EducationForm />}
-          {step === 4 && <CoursesForm />}
-        </div>
-      </Card>
+      {isLoading && (
+        <Loading
+          title={loadingCreate[lang].title}
+          description={loadingCreate[lang].description}
+        />
+      )}
+      {!isLoading && !isGenerate && (
+        <Card>
+          <Stepper currentStep={step} />
+          <div className="flex flex-col p-2 md:p-6">
+            {step === 1 && <PersonalForm />}
+            {step === 2 && <ExperienceForm />}
+            {step === 3 && <EducationForm />}
+            {step === 4 && <ExtraForm />}
+          </div>
+        </Card>
+      )}
+      {!isLoading && isGenerate && (
+        <Card>
+          <GeneratePdf />
+        </Card>
+      )}
     </div>
   );
 };
